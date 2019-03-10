8ba3c06dcfbafbbe68f97af01a84e205ed090e72
Ayoub Malek <kogito@hotmail.fr>
2019-03-10 01:50:52 +0100

    update setup file

--- setup.py
+++ setup.py
@@ -2,9 +2,17 @@
 
 from setuptools import setup, find_packages
 
+f = open('README.md', 'r')
+try:
+    long_desc = f.read()
+finally:
+    f.close()
+
+requires = ['Sphinx>=0.6']
+
 setup (
-        name         = 'sphinx-pdfembed',
-        version      = '1.0',
+        name         = 'sphinxcontrib-pdfembed',
+        version      = '0.1',
         author       = 'Super Kogito',
         author_email = 'superkogito@gmail.com',
         description  = 'Sphinx extension to embedd a pdf files webpages',
@@ -12,8 +20,18 @@ setup (
         url          = 'https://github.com/SuperKogito/sphinx-pdfembed',
         packages     = find_packages(),
         classifiers  = [
-                        "Programming Language :: Python :: 3",
-                        "License :: OSI Approved :: MIT License",
-                        "Operating System :: OS Independent",
-                       ],        
+                        'Development Status :: 3 - Alpha',
+                        'Environment :: Console',
+                        'Environment :: Web Environment',
+                        'Intended Audience :: Developers',
+                        'License :: OSI Approved :: BSD License',
+                        'Operating System :: OS Independent',
+                        'Programming Language :: Python',
+                        'Topic :: Documentation',
+                        'Topic :: Utilities',
+                      ],
+        platforms            = 'any',
+        include_package_data = True,
+        install_requires     = requires,
+        namespace_packages   = ['sphinxcontrib'],
      )
